3eaa2be810026291bdd7856aa14bb5bbd6f07336
Pavel <iverick091@gmail.com>
2025-01-24 17:09:31 +0000

    Add basic animals.txt file and function to read it's data in main class.

--- src/main/java/com/mycompany/ca1_programming/CA1_Programming.java
+++ src/main/java/com/mycompany/ca1_programming/CA1_Programming.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.ca1_programming;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Paths;
+
 /**
  *
  * @author User
@@ -11,6 +15,28 @@ package com.mycompany.ca1_programming;
 public class CA1_Programming {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String inputFilePath = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "animals.txt").toAbsolutePath().toString();
+        System.out.println(inputFilePath);
+        
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFilePath))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] speciesNameLine = line.split(",");
+
+                String species = speciesNameLine[0];
+                String name = speciesNameLine[1];
+                String habitat = bufferedReader.readLine();
+                
+                String[] dobWeightLine = bufferedReader.readLine().split(",");
+                String dob = dobWeightLine[0];
+                String weight = dobWeightLine[1];
+                
+                String[] typeLine = bufferedReader.readLine().split(",");
+                String type = typeLine[0];
+                System.out.println(type);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
